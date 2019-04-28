a828005e1cc97648eddca34624e4dec98aa017ab
edoardo <realini.edoardo@gmail.com>
2019-04-28 19:42:04 +0200

    aggiornato MapBuiderTest

--- src/test/java/model/map/MapBuilderTest.java
+++ src/test/java/model/map/MapBuilderTest.java
@@ -14,12 +14,16 @@ class MapBuilderTest {
     private MapBuilder builderTest;
     private Map mapTest1;
     private Map mapTest2;
+    private Map mapTest3;
+    private Map mapTest4;
 
     @BeforeEach
     void setUp() {
         builderTest = new MapBuilder();
         mapTest1 = new Map();
         mapTest2 = new Map();
+        mapTest3 = new Map();
+        mapTest4 = new Map();
     }
 
     @Test
@@ -36,6 +40,19 @@ class MapBuilderTest {
             e.printStackTrace();
         }
 
+        try {
+            mapTest3 = builderTest.makeMap(3);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+        try {
+            mapTest4 = builderTest.makeMap(4);
+        }catch(IOException e){
+            e.printStackTrace();
+        }
+
+
         assertEquals(mapTest1.getMapID(), 1);
         assertEquals(mapTest1.getSquaresMatrix()[0][0].getActiveStatus(), false);
         assertEquals(mapTest1.getSquaresMatrix()[0][1].getActiveStatus(), true);
@@ -50,6 +67,8 @@ class MapBuilderTest {
 
         assertEquals(mapTest1.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.UP, Directions.RIGHT));
         assertNotEquals(mapTest1.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.LEFT, Directions.RIGHT));
+
+        System.out.println("map1 tested OK");
 
         assertEquals(mapTest2.getMapID(), 2);
         assertEquals(mapTest2.getSquaresMatrix()[0][0].getActiveStatus(), true);
@@ -67,6 +86,57 @@ class MapBuilderTest {
         assertEquals(mapTest2.getSquaresMatrix()[0][0].getAllowedMoves(),Arrays.asList(Directions.UP, Directions.RIGHT));
         assertEquals(mapTest2.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.UP, Directions.DOWN));
         assertNotEquals(mapTest2.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.LEFT, Directions.RIGHT));
+
+        System.out.println("map2 tested OK");
+
+        assertEquals(mapTest3.getMapID(), 3);
+        assertEquals(mapTest3.getSquaresMatrix()[0][0].getActiveStatus(), false);
+        assertEquals(mapTest3.getSquaresMatrix()[0][1].getActiveStatus(), true);
+        assertEquals(mapTest3.getSquaresMatrix()[0][2].getActiveStatus(), true);
+
+        assertEquals(mapTest3.getSquaresMatrix()[0][1].getColor(), Color.RED);
+        assertEquals(mapTest3.getSquaresMatrix()[0][2].getColor(), Color.BLUE);
+
+        assertEquals(mapTest3.getSquaresMatrix()[1][0].getColor(), Color.WHITE);
+        assertEquals(mapTest3.getSquaresMatrix()[1][1].getColor(), Color.RED);
+        assertEquals(mapTest3.getSquaresMatrix()[1][2].getColor(), Color.BLUE);
+
+        assertEquals(mapTest3.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.UP, Directions.RIGHT));
+        assertNotEquals(mapTest3.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.LEFT, Directions.RIGHT));
+
+        System.out.println("map3 tested OK");
+
+        assertEquals(mapTest3.getMapID(), 3);
+        assertEquals(mapTest3.getSquaresMatrix()[0][0].getActiveStatus(), false);
+        assertEquals(mapTest3.getSquaresMatrix()[0][1].getActiveStatus(), true);
+        assertEquals(mapTest3.getSquaresMatrix()[0][2].getActiveStatus(), true);
+
+        assertEquals(mapTest3.getSquaresMatrix()[0][1].getColor(), Color.RED);
+        assertEquals(mapTest3.getSquaresMatrix()[0][2].getColor(), Color.BLUE);
+
+        assertEquals(mapTest3.getSquaresMatrix()[1][0].getColor(), Color.WHITE);
+        assertEquals(mapTest3.getSquaresMatrix()[1][1].getColor(), Color.RED);
+        assertEquals(mapTest3.getSquaresMatrix()[1][2].getColor(), Color.BLUE);
+
+        assertEquals(mapTest3.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.UP, Directions.RIGHT));
+        assertNotEquals(mapTest3.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.LEFT, Directions.RIGHT));
+
+        System.out.println("map4 tested OK");
+
+        assertEquals(mapTest4.getMapID(), 4);
+        assertEquals(mapTest4.getSquaresMatrix()[0][0].getActiveStatus(), true);
+        assertEquals(mapTest4.getSquaresMatrix()[0][1].getActiveStatus(), true);
+        assertEquals(mapTest4.getSquaresMatrix()[0][2].getActiveStatus(), true);
+
+        assertEquals(mapTest4.getSquaresMatrix()[0][1].getColor(), Color.RED);
+        assertEquals(mapTest4.getSquaresMatrix()[0][0].getColor(), Color.WHITE);
+
+        assertEquals(mapTest4.getSquaresMatrix()[1][0].getColor(), Color.WHITE);
+        assertEquals(mapTest4.getSquaresMatrix()[1][1].getColor(), Color.PURPLE);
+        assertEquals(mapTest4.getSquaresMatrix()[1][2].getColor(), Color.BLUE);
+
+        assertEquals(mapTest4.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.UP, Directions.DOWN));
+        assertNotEquals(mapTest4.getSquaresMatrix()[0][1].getAllowedMoves(),Arrays.asList(Directions.LEFT, Directions.RIGHT));
 
     }
 
