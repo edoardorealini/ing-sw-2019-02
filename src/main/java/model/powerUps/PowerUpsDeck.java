d47d506f31694a61006d2d843f21bd1ad185fb3a
GioValca <valcarenghi.valcarenghi@gmail.com>
2019-04-30 15:45:41 +0200

    cambio nomi powerDeck

--- src/main/java/model/powerUps/PowerUpsDeck.java
+++ src/main/java/model/powerUps/PowerUpsDeck.java
@@ -1,35 +1,40 @@
 package model.powerUps;
 
-import java.util.ArrayList;
+import java.util.*;
 
 public class PowerUpsDeck {
-    private ArrayList<PowerUps> powerUpsDeck;
+    private LinkedList<PowerUps> powerUpsDeck;
 
     public PowerUpsDeck(){
 
-        powerUpsDeck.add(new PowerUps('B', "Granata Venom"));
-        powerUpsDeck.add(new PowerUps('R', "Granata Venom"));
-        powerUpsDeck.add(new PowerUps('Y', "Granata Venom"));
-        powerUpsDeck.add(new PowerUps('B', "Mirino"));
-        powerUpsDeck.add(new PowerUps('R', "Mirino"));
-        powerUpsDeck.add(new PowerUps('Y', "Mirino"));
-        powerUpsDeck.add(new PowerUps('B', "Raggio cinetico"));
-        powerUpsDeck.add(new PowerUps('R', "Raggio cinetico"));
-        powerUpsDeck.add(new PowerUps('Y', "Raggio cinetico"));
-        powerUpsDeck.add(new PowerUps('B', "Teletrasporto"));
-        powerUpsDeck.add(new PowerUps('R', "Teletrasporto"));
-        powerUpsDeck.add(new PowerUps('Y', "Teletrasporto"));
+        powerUpsDeck.add(new PowerUps('B', "TagBack Grenade"));
+        powerUpsDeck.add(new PowerUps('R', "TagBack Grenade"));
+        powerUpsDeck.add(new PowerUps('Y', "TagBack Grenade"));
+        powerUpsDeck.add(new PowerUps('B', "Targeting Scope"));
+        powerUpsDeck.add(new PowerUps('R', "Targeting Scope"));
+        powerUpsDeck.add(new PowerUps('Y', "Targeting Scope"));
+        powerUpsDeck.add(new PowerUps('B', "Newton"));
+        powerUpsDeck.add(new PowerUps('R', "Newton"));
+        powerUpsDeck.add(new PowerUps('Y', "Newton"));
+        powerUpsDeck.add(new PowerUps('B', "Teleporter"));
+        powerUpsDeck.add(new PowerUps('R', "Teleporter"));
+        powerUpsDeck.add(new PowerUps('Y', "Teleporter"));
 
+    }
+
+    public void shuffle(){
+        Collections.shuffle(powerUpsDeck);
     }
 
 
     public PowerUps removePowerUps(){
-         return powerUpsDeck.get(0);
+         return powerUpsDeck.getFirst();
          //TODO come cancello la prima cella ?
 
     }
 
     public void addPowerUps(PowerUps powerUps){
-        powerUpsDeck.add(powerUpsDeck.size()-1,powerUps);
+        powerUpsDeck.addLast(powerUps);
     }
+
 }
