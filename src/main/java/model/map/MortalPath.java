a6f2838dd4acb0d7b2855d0504cc49ef726dcab2
edoardo <realini.edoardo@gmail.com>
2019-04-30 15:47:45 +0200

    modificato versione jetbrains annotations, aggiunta classe MatchController vuota e modificato classe MortalPath minors changes

--- src/main/java/model/map/MortalPath.java
+++ src/main/java/model/map/MortalPath.java
@@ -1,17 +1,15 @@
 package model.map;
 
-import java.util.ArrayList;
-import java.util.List;	  //NB: we have to use a List because we need to know the order of kills in case of draw
+import java.util.*;	  //NB: we have to use a List because we need to know the order of kills in case of tie
 
 public class MortalPath {
-	private List<Integer> mortalShots;  //id players or should we use an array of Color?
+	private List<Integer> mortalShots;
 	private int skulls;
-	private List<Integer> doubleKill;    //same comment as above
+	private List<Integer> doubleKill;
 
 	public MortalPath() {
 		mortalShots = new ArrayList<>();
 		doubleKill = new ArrayList<>();
-		//we have to initialize "skulls" with the number choosen by the first model.player (e.g. 5 or 8)
 	}
 
 	public List<Integer> getMortalShots() {
@@ -26,11 +24,12 @@ public class MortalPath {
 		return skulls;
 	}
 
-	public void startLastRound() {
-		//TODO
+	public void setSkulls(int skulls) {
+		this.skulls = skulls;
+		//TODO aggiornare valore al primo turno
 	}
 
-	public void kill(int shots, int idPlayer) {		//shots can be 1 or 2, depending on the rage damage
+	public void setMortalShots(int shots, int idPlayer){		//shots can be 1 or 2, depending on the rage damage
 		mortalShots.add(idPlayer);
 		if (shots == 2) mortalShots.add(idPlayer);
 	}
