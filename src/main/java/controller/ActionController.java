bf0368ec5ca6b76a1cacda4ff24d9980c8d88a53
GioValca <valcarenghi.valcarenghi@gmail.com>
2019-04-30 16:34:01 +0200

    -_-

---
+++ src/main/java/controller/ActionController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.player.Player;
+
+public  abstract class ActionController {
+    private Player player;
+
+    public abstract Player getPlayer();
+
+
+}
---
+++ src/main/java/controller/GrabController.java
@@ -0,0 +1,11 @@
+package controller;
+
+import model.player.Player;
+
+public class GrabController extends ActionController {
+
+    @Override
+    public Player getPlayer() {
+        return null;
+    }
+}
