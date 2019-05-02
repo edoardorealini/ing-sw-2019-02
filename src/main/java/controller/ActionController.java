9a423dc7e27d3521f9fd6610219962189905601d
Madsomma <48261091+Madsomma@users.noreply.github.com>
2019-05-02 10:35:42 +0200

    Update ActionController.java
    imported class Map from package map

--- src/main/java/controller/ActionController.java
+++ src/main/java/controller/ActionController.java
@@ -1,10 +1,11 @@
 package controller;
 
+import model.map.Map;
 import model.player.Player;
 
 public  abstract class ActionController {
     protected Player player;
-    protected Map
+    protected Map map;
 
     public abstract Player getPlayer();
 
