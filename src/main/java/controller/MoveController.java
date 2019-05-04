faff8fe968056304fa8b1fe1bbdd67fd4f125e8b
edoardo <realini.edoardo@gmail.com>
2019-05-04 19:00:34 +0200

    aggiunta nuova Exception e modificato classe MoveController

--- src/main/java/controller/MoveController.java
+++ src/main/java/controller/MoveController.java
@@ -1,4 +1,30 @@
 package controller;
 
+import model.Match;
+import model.map.Directions;
+import model.player.Player;
+import model.map.*;
+
 public class MoveController {
+    private Match match;
+
+    /*
+        Con il costruttore genero l'oggetto con già dentro il riferimento al match che viene istanziato a livelo di
+        match controller
+    */
+    public MoveController(Match realMatch){
+        this.match = realMatch;
+    }
+
+    /*
+        moveOneSquare changes the player position according to the input direction
+        throws an exception - NotAllowedMoveException
+
+    */
+
+    public void moveOneSquare(Directions direction, Player player){
+        //TODO
+    }
+
+
 }
---
+++ src/main/java/exception/NotAllowedMoveException.java
@@ -0,0 +1,5 @@
+package exception;
+
+public class NotAllowedMoveException extends Exception{
+
+}
