628b35bf356ecba204136ddce252acd7d5ba5a7d
Madsomma <48261091+Madsomma@users.noreply.github.com>
2019-05-04 16:22:56 +0200

    Update ShootController.java

--- src/main/java/controller/ShootController.java
+++ src/main/java/controller/ShootController.java
@@ -1,23 +1,27 @@
 package controller;
 
 import controller.ActionController;
+import jdk.nashorn.internal.runtime.arrays.ArrayLikeIterator;
 import model.Match;
 import model.player.Player;
+
+import java.util.ArrayList;
 
 public class ShootController extends ActionController {
 
 	private Match match;
+	private ArrayList<Player> targetPlayers;
+
 
 	public Player getPlayer() {
 		return match.getCurrentPlayer();
 	}
 
-	/*
-	implementazione del metodo astratto dalla classe abstract.
- 	*/
+	//implementazione del metodo astratto dalla classe abstract.
 	public Match getMatch() {
 		return match;
 	}
 
-	//TODO where should I put the array of the target players and the relatives methods? Here or in Match (model class)?
+
+
 }
