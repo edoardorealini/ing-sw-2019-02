d55ee233599e905af43a90a3c3cb08647522f122
edoardo <realini.edoardo@gmail.com>
2019-05-07 20:12:34 +0200

    corretto errore in una classe di powerup

--- src/main/java/model/powerup/Newton.java
+++ src/main/java/model/powerup/Newton.java
@@ -5,6 +5,6 @@ import model.Color;
 public class Newton extends PowerUp{
 
     public Newton(Color color){
-        super(color, PowerUpName.TARGETING_SCOPE);
+        super(color, PowerUpName.NEWTON);
     }
 }
