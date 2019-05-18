d9a58d120f024fa62a3fc8cd3978a91cb90434c3
edoardo <realini.edoardo@gmail.com>
2019-05-18 15:02:14 +0200

    aggiunta possibilità di mandare più messaggi dal client al server

--- src/main/java/client/ClientStarter.java
+++ src/main/java/client/ClientStarter.java
@@ -4,17 +4,23 @@ import controller.MatchControllerInterface;
 
 import java.rmi.registry.LocateRegistry;
 import java.rmi.registry.Registry;
+import java.util.Scanner;
 
-public class ClientStarter {
+public class ClientStarter{
 
     public static void main(String[] args) {
         try {
             Registry registry = LocateRegistry.getRegistry(1338);
             MatchControllerInterface matchController = (MatchControllerInterface) registry.lookup("matchController");
 
-            String response = matchController.RMICallTest("Hello");
+            Scanner input = new Scanner(System.in);
 
-            System.out.println("Response from server: " + response);
+            while(true) {
+                System.out.println("\nInsert message to send to the server:");
+                String message = input.nextLine();
+                String response = matchController.RMICallTest(message);
+                System.out.println("Response from server: " + response);
+            }
 
         }
         catch (Exception e){
