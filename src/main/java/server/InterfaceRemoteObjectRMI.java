6350b1a5ba666525fe7e1f65e365e8f1ba0d6f85
GioValca <valcarenghi.valcarenghi@gmail.com>
2019-05-19 19:16:23 +0200

    sistemato RMI

--- src/main/java/server/InterfaceRemoteObjectRMI.java
+++ src/main/java/server/InterfaceRemoteObjectRMI.java
@@ -23,22 +23,22 @@ public interface InterfaceRemoteObjectRMI extends Remote{
     void buildMap(int mapID) throws RemoteException;
 
     //metodi derivanti da classe moveController
-    void move(Player player, Square destination, int maxDistanceAllowed) throws Exception, RemoteException;
+    void move(Player player, int iDestination, int jDestination, int maxDistanceAllowed) throws Exception, RemoteException;
 
-    boolean isAllowedMove(Square startingPoint, Square destination, int maxDistance) throws RemoteException;
+    boolean isAllowedMove(Square startingPoint, int iDestination, int jDestination, int maxDistance) throws RemoteException;
 
-    void moveOneSquare(Directions direction) throws  Exception, RemoteException;
+    void moveOneSquare(String movement) throws  Exception, RemoteException;
 
-    void moveOneSquare(Directions direction, Player player) throws Exception, RemoteException;
+    void moveOneSquare(String movement, Player player) throws Exception, RemoteException;
 
 
     //metodi da grab controller
     void grabAmmoCard() throws Exception, RemoteException;
 
-    void grabWeapon(Weapon weapon) throws Exception, RemoteException;
+    void grabWeapon(int indexOfWeapon) throws Exception, RemoteException;
 
     //metodi di powerUpController
-    void usePowerUpAsAmmo(PowerUp powerUp) throws Exception, RemoteException;
+    void usePowerUpAsAmmo(int indexOfPowerUp) throws Exception, RemoteException;
 
     void useTeleporter(PowerUp teleporter, Square destination) throws RemoteException;
 
