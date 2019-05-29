b353502ee0f83715accddcb506c8388100de9e84
edoardo <realini.edoardo@gmail.com>
2019-05-29 10:02:55 +0200

    Corretto problema GUI. Errore nell'utilizzo di cotruttore di Image! (ho scritto un po di commenti così si capisce meglio)

--- src/main/java/client/GUI/ImageViewer.java
+++ src/main/java/client/GUI/ImageViewer.java
@@ -6,7 +6,8 @@ import javafx.scene.image.ImageView;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
 
-import javafx.scene.image.Image ;
+import javafx.scene.image.Image;
+
 import java.io.File;
 
 public class ImageViewer extends Application {
@@ -20,12 +21,23 @@ public class ImageViewer extends Application {
 
         primaryStage.setTitle("Adrenaline");
 
-        Image image = new Image(File.separatorChar + "src" + File.separatorChar + "main"
-                + File.separatorChar + "resources" + File.separatorChar + "Adrenaline_1024x1024@2x.png");
+
+        /*
+        Bella johnny, qui ti metto esempio con URL da internet, funziona
+        Image image = new Image("https://picsum.photos/id/117/200/300");
+        */
+
+        //qui sotto ti metto il modo che devi usare per prendere l'immagine da file
+        //per prima cosa devi aprire il file con new File, poi renderlo leggibile (.toURI().toString())
+
+        File file = new File("." + File.separatorChar + "src" + File.separatorChar + "main"
+                + File.separatorChar + "resources" + File.separatorChar + "AdrenalineBackground.png");
+        Image image = new Image(file.toURI().toString());
+
         GridPane grid = new GridPane();
         grid.getChildren().add(new ImageView(image));
 
-        Scene scene = new Scene(grid,1024,1024);
+        Scene scene = new Scene(grid,996,698);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
