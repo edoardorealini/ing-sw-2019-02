ddcc5f8f9efbf0765e1e2dfbcb19026a46197497
GioValca <valcarenghi.valcarenghi@gmail.com>
2019-06-04 23:18:51 +0200

    Main Page prove

--- src/main/java/client/GUI/MainPage.java
+++ src/main/java/client/GUI/MainPage.java
@@ -1,12 +1,25 @@
 package client.GUI;
 
+import com.sun.javafx.scene.SceneEventDispatcher;
 import javafx.application.Application;
+import javafx.beans.property.ObjectProperty;
+import javafx.beans.property.SimpleObjectProperty;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.stage.Stage;
 import model.Match;
-
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
 import java.io.File;
 
 public class MainPage extends Application {
@@ -15,31 +28,34 @@ public class MainPage extends Application {
     @Override
     public void start(Stage mainStage) throws Exception {
         mainStage.setTitle("Adrenaline");
-        BorderPane borderPane = new BorderPane();
-        VBox vbox= new VBox();
-        borderPane.setLeft(vbox);
-        GridPane gridPane = new GridPane();
-        borderPane.setCenter(gridPane);
-
-
+        /*// load images
         File file = new File("." + File.separatorChar + "src" + File.separatorChar + "main"
-                + File.separatorChar + "resources" + File.separatorChar + "Vita1.png");
-
-        BackgroundImage myBI= new BackgroundImage(new Image(file.toURI().toString()),
-                BackgroundRepeat.REPEAT, BackgroundRepeat.NO_REPEAT, BackgroundPosition.DEFAULT,
-                BackgroundSize.DEFAULT);
-        vbox.setBackground(new Background(myBI));
-
-        Scene scene = new Scene(borderPane,996,698);
+                + File.separatorChar + "resources" + File.separatorChar + "lifeBoards" +File.separatorChar + "Vita1.png");
+        Image image = new Image(file.toURI().toString());
+        ImageView iv = new ImageView(image);
+        StackPane stackPane = new StackPane(iv);
+        stackPane.setMaxHeight(100);
+        stackPane.setMaxWidth(20);
+        iv.fitWidthProperty().bind(stackPane.widthProperty());
+        VBox vBox = new VBox();
+        vBox.getChildren().add(stackPane);
+        Scene scene = new Scene(vBox,1400,825);
         mainStage.setScene(scene);
-        mainStage.setMaxWidth(996);
-        mainStage.setMaxHeight(698);
-        mainStage.show();
+        mainStage.setScene(scene);
+        mainStage.sizeToScene();
+        mainStage.show();  */
+        
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
     public void setMatch(Match match) {
         this.match = match;
     }
+
 }
 
 
