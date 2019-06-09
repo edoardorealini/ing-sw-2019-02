642797c5c2924cd8c8f5b64ad680812dedcff5cd
Madsomma <48261091+Madsomma@users.noreply.github.com>
2019-06-10 01:15:43 +0200

    finished the weapon popup :adoss:

--- src/main/java/client/GUI/popUpWeapons/GeneralWeaponPopUp.java
+++ src/main/java/client/GUI/popUpWeapons/GeneralWeaponPopUp.java
@@ -9,10 +9,12 @@ import javafx.scene.control.Label;
 import javafx.scene.control.SplitPane;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.Match;
+import model.map.Directions;
 import model.player.Player;
 import model.weapons.Weapon;
 
@@ -37,29 +39,57 @@ public class GeneralWeaponPopUp extends Application {
         ImageView iv = new ImageView(image);
         iv.setX(0);
         iv.setY(0);
-        iv.setFitHeight(250);
-        iv.setFitWidth(200);
+        iv.setFitHeight(300);
+        iv.setFitWidth(250);
         iv.setPreserveRatio(true);
 
+
+        //creating all labels and choice boxes
         VBox vBox = new VBox();
         splitPane.getItems().addAll(iv,vBox);
 
-        Label shootingMode = new Label("Shooting mode: ");
+        HBox modesHbox = new HBox();
+        HBox targetHbox = new HBox();
+        HBox squaresHbox = new HBox();
+
+        Label shootingMode = new Label("Select the shooting modes: ");
+        Label targets = new Label("Select the targets: ");
+        Label squares = new Label("Select the squares by indexes (in case of more than one, the first is where you want to move the target): ");
+        Label direction = new Label("Select the direction: ");
+        Label damageBeforeMove = new Label("Do you want to execute the optional effect before moving the target? ");
+        Label emptySpace= new Label(" ");
+        Label emptySpace1= new Label(" ");
+
         ChoiceBox<String> choiceBoxEffect = new ChoiceBox<>();
         ChoiceBox<String> choiceBoxEffectOpt1 = new ChoiceBox<>();
         ChoiceBox<String> choiceBoxEffectOpt2 = new ChoiceBox<>();
+        ChoiceBox<String> target1 = new ChoiceBox<>();
+        ChoiceBox<String> target2 = new ChoiceBox<>();
+        ChoiceBox<String> target3 = new ChoiceBox<>();
+        ChoiceBox<Integer> xSquare1 = new ChoiceBox<>();
+        ChoiceBox<Integer> xSquare2 = new ChoiceBox<>();
+        ChoiceBox<Integer> ySquare1 = new ChoiceBox<>();
+        ChoiceBox<Integer> ySquare2 = new ChoiceBox<>();
+        ChoiceBox<Directions> directionBox = new ChoiceBox<>();
+        ChoiceBox<Boolean> damageBeforeMoveBox = new ChoiceBox<>();
 
+        //start filling the vbox
+        vBox.getChildren().add(shootingMode);
+
+        //here it is build the hbox containing the shooting modes
         switch (weapon.getRequiredParameters().getShootModeType()) {
 
             case 1:
                 choiceBoxEffect.getItems().add("Basic");
                 choiceBoxEffect.setValue("Basic");
+                modesHbox.getChildren().add(choiceBoxEffect);
                 break;
 
             case 2:
                 choiceBoxEffect.getItems().add("Basic");
                 choiceBoxEffect.getItems().add("Alternate");
                 choiceBoxEffect.setValue("Basic");
+                modesHbox.getChildren().add(choiceBoxEffect);
                 break;
 
             case 3:
@@ -68,7 +98,7 @@ public class GeneralWeaponPopUp extends Application {
                 choiceBoxEffect.setValue("Basic");
                 choiceBoxEffectOpt1.getItems().add("Basic");
                 choiceBoxEffectOpt1.getItems().add("Optional one");
-                // choiceBoxEffectOpt1.setValue(null);  TODO how to set it as default?
+                modesHbox.getChildren().addAll(choiceBoxEffect, choiceBoxEffectOpt1);
                 break;
 
             case 4:
@@ -82,29 +112,108 @@ public class GeneralWeaponPopUp extends Application {
                 choiceBoxEffectOpt2.getItems().add("Basic");
                 choiceBoxEffectOpt2.getItems().add("Optional one");
                 choiceBoxEffectOpt2.getItems().add("Optional two");
+                modesHbox.getChildren().addAll(choiceBoxEffect, choiceBoxEffectOpt1, choiceBoxEffectOpt2);
                 break;
 
             default:
                 break;
         }
 
-        Label target = new Label("Chose the target: ");
-        ChoiceBox<String> choiceBoxName = new ChoiceBox<>();
-      //  fillChoiceBoxName(choiceBoxName);
+        vBox.getChildren().add(modesHbox);
 
+
+        //here it is build the hbox containing the targets
+        switch (weapon.getRequiredParameters().getNumberOfTargets()) {
+            case 0:
+                break;
+
+            case 1:
+                vBox.getChildren().add(targets);
+                fillChoiceBoxName(target1);
+                targetHbox.getChildren().add(target1);
+                break;
+
+            case 2:
+                vBox.getChildren().add(targets);
+                fillChoiceBoxName(target1);
+                fillChoiceBoxName(target2);
+                targetHbox.getChildren().addAll(target1, target2);
+                break;
+
+            case 3:
+                vBox.getChildren().add(targets);
+                fillChoiceBoxName(target1);
+                fillChoiceBoxName(target2);
+                fillChoiceBoxName(target3);
+                targetHbox.getChildren().addAll(target1, target2, target3);
+                break;
+
+            default:
+                break;
+        }
+
+        vBox.getChildren().add(targetHbox);
+
+
+        //here it is build the hbox containing the squares
+        switch (weapon.getRequiredParameters().getNumberOfSquares()) {
+            case 0:
+                break;
+
+            case 1:
+                vBox.getChildren().add(squares);
+                setX(xSquare1);
+                setY(ySquare1);
+                squaresHbox.getChildren().addAll(xSquare1, ySquare1);
+                break;
+
+            case 2:
+                vBox.getChildren().add(squares);
+                setX(xSquare1);
+                setY(ySquare1);
+                setX(xSquare2);
+                setY(ySquare2);
+                squaresHbox.getChildren().addAll(xSquare1, ySquare1, xSquare2, ySquare2);
+                break;
+
+            default:
+                break;
+        }
+
+        vBox.getChildren().add(squaresHbox);
+
+        if (weapon.getRequiredParameters().needToKnowDirection()) {
+            vBox.getChildren().add(direction);
+            directionBox.getItems().addAll(Directions.UP, Directions.DOWN, Directions.LEFT, Directions.RIGHT);
+            directionBox.setValue(Directions.UP);
+            vBox.getChildren().add(directionBox);
+        }
+
+        if (weapon.getRequiredParameters().needToKnowDamageBeforeMove()) {
+            vBox.getChildren().add(damageBeforeMove);
+            damageBeforeMoveBox.getItems().addAll(true, false);
+            damageBeforeMoveBox.setValue(false);
+            vBox.getChildren().add(damageBeforeMoveBox);
+        }
+
+
+        //building the button and setting the action
         Button shootButton = new Button(" SHOOT ");
-        shootButton.setTextFill(Color.RED);
-        shootButton.setAlignment(Pos.BOTTOM_CENTER);
-        shootButton.setOnAction(e -> Shoot(choiceBoxEffect, choiceBoxName));
+        shootButton.setTextFill(Color.BLUE);
+        shootButton.setAlignment(Pos.CENTER);
+        //shootButton.setOnAction(e -> Shoot(choiceBoxEffect, ));
 
-        Label emptySpace= new Label(" ");
-        Label emptySpace1= new Label(" ");
-
-        vBox.getChildren().addAll(shootingMode, choiceBoxEffect, choiceBoxEffectOpt1, choiceBoxEffectOpt2, target, choiceBoxName, emptySpace, emptySpace1, shootButton);
+        vBox.getChildren().addAll(emptySpace, emptySpace1, shootButton);
         vBox.setAlignment(Pos.CENTER);
-        vBox.setSpacing(5);
+        vBox.setSpacing(8);
+        targetHbox.setAlignment(Pos.CENTER);
+        modesHbox.setAlignment(Pos.CENTER);
+        squaresHbox.setAlignment(Pos.CENTER);
+        targetHbox.setSpacing(5);
+        modesHbox.setSpacing(5);
+        squaresHbox.setSpacing(5);
 
-        Scene scene= new Scene(splitPane,300,250);
+        Scene scene = new Scene(splitPane,500,300);
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -130,5 +239,16 @@ public class GeneralWeaponPopUp extends Application {
     public void setMatch(Match match) {
         this.match = match;
     }
+
+    private void setX(ChoiceBox<Integer> xChoiceBox) {
+        xChoiceBox.getItems().addAll(0, 1, 2, 3);
+        xChoiceBox.setValue(0);
+    }
+
+    private void setY(ChoiceBox<Integer> yChoiceBox) {
+        yChoiceBox.getItems().addAll(0, 1, 2);
+        yChoiceBox.setValue(0);
+    }
+
 
 }
