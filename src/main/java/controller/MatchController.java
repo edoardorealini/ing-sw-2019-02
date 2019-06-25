75da237558becce221ecd9a4c25b58ffd8198d18
edoardorealini <realini.edoardo@gmail.com>
2019-06-25 09:51:40 +0200

    Correzione EndOfTurn allo scadere del timer del turno.

--- src/main/java/controller/MatchController.java
+++ src/main/java/controller/MatchController.java
@@ -599,16 +599,18 @@ public class MatchController{
                             Player player = match.getCurrentPlayer();
                             //if i enter this timer it means that the player who launched it hasn't finished his turn
                             System.out.println("[TURNTIMER]: The player " + player.getNickname() + " has expired his time to complete the turn, skipping his turn . . .");
+                            /*
                             player.getStatus().setTurnStatusWaitTurn();
                             setNewCurrentPlayer();
-
                             try {
                                 serverControllerRMI.askRespawn();
                                 //questo serve solo per il primo turno, ovvero per gestire la prima spawn, in teoria poi non crea problemi. (TO TEST)
                             } catch (Exception e) {
                                 e.printStackTrace();
                             }
-
+                            */
+                            player.getStatus().setTurnStatusEndTurn();
+                            goToNextStatus(player);
 
                         }
                     };
@@ -689,17 +691,19 @@ public class MatchController{
                                 Player player = match.getCurrentPlayer();
                                 //if i enter this timer it means that the player who launched it hasn't finished his turn
                                 System.out.println("[TURNTIMER]: The player " + player.getNickname() + " has expired his time to complete the turn, skipping his turn . . .");
+                                /*
                                 player.getStatus().setTurnStatusWaitTurn();
                                 setNewCurrentPlayer();
-
                                 try {
                                     serverControllerRMI.askRespawn();
                                     //questo serve solo per il primo turno, ovvero per gestire la prima spawn, in teoria poi non crea problemi. (TO TEST)
                                 } catch (Exception e) {
                                     e.printStackTrace();
                                 }
+                                */
 
-
+                                player.getStatus().setTurnStatusEndTurn();
+                                goToNextStatus(player);
                             }
                         };
                         turnTimer.schedule(turnTimerTask, turnDuration);
