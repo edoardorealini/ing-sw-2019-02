082ad2ea751c519e9822ea654eaf77a3af3852cf
GioValca <valcarenghi.valcarenghi@gmail.com>
2019-06-27 22:53:35 +0200

    changed font

--- src/main/java/client/GUI/Ranking.java
+++ src/main/java/client/GUI/Ranking.java
@@ -5,7 +5,12 @@ import javafx.application.Application;
 import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.effect.Blend;
+import javafx.scene.effect.BlendMode;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.effect.InnerShadow;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import model.Match;
 import java.util.*;
@@ -42,10 +47,72 @@ public class Ranking extends Application {
             listPlayersLabel.get(i).setText("Player "+match.getPlayers().get(i).getNickname()+"->  total score : "+match.getPlayers().get(i).getPoints());
         }
 
+        // ++++++++++++++++++++++++++++++++++
+        // codice per cambiare font
+        player1.setTextFill(Color.YELLOWGREEN);
+        player1.setStyle("-fx-font-weight: bold");
+        player2.setTextFill(Color.YELLOWGREEN);
+        player2.setStyle("-fx-font-weight: bold");
+        player3.setTextFill(Color.YELLOWGREEN);
+        player3.setStyle("-fx-font-weight: bold");
+        player4.setTextFill(Color.YELLOWGREEN);
+        player4.setStyle("-fx-font-weight: bold");
+        player5.setTextFill(Color.YELLOWGREEN);
+        player5.setStyle("-fx-font-weight: bold");
+
+        Blend blend = new Blend();
+        blend.setMode(BlendMode.MULTIPLY);
+
+        DropShadow ds = new DropShadow();
+        ds.setColor(Color.rgb(254, 235, 66, 0.3));
+        ds.setOffsetX(5);
+        ds.setOffsetY(5);
+        ds.setRadius(5);
+        ds.setSpread(0.2);
+
+        blend.setBottomInput(ds);
+
+        DropShadow ds1 = new DropShadow();
+        ds1.setColor(Color.web("#f13a00"));
+        ds1.setRadius(20);
+        ds1.setSpread(0.2);
+
+        Blend blend2 = new Blend();
+        blend2.setMode(BlendMode.MULTIPLY);
+
+        InnerShadow is = new InnerShadow();
+        is.setColor(Color.web("#feeb42"));
+        is.setRadius(9);
+        is.setChoke(0.8);
+        blend2.setBottomInput(is);
+
+        InnerShadow is1 = new InnerShadow();
+        is1.setColor(Color.web("#f13a00"));
+        is1.setRadius(5);
+        is1.setChoke(0.4);
+        blend2.setTopInput(is1);
+
+        Blend blend1 = new Blend();
+        blend1.setMode(BlendMode.MULTIPLY);
+        blend1.setBottomInput(ds1);
+        blend1.setTopInput(blend2);
+
+        blend.setTopInput(blend1);
+
+        player1.setEffect(blend);
+        player2.setEffect(blend);
+        player3.setEffect(blend);
+        player4.setEffect(blend);
+        player5.setEffect(blend);
+
+        // +++++++++++++++++++++++++++++++++++++++
+
         vBox.setAlignment(Pos.CENTER);
         Scene scene = new Scene(vBox,300,300);
         primaryStage.setScene(scene);
         primaryStage.show();
+
+
     }
 
     public void setMatch(Match match) {
